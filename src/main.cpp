f158125e4d205629eede64ea9508d51eedaa7c1e
Scott-Bunting <scott.bunting15@imperial.ac.uk>
2019-02-06 21:27:27 +0000

    Change main.cpp file to read and display GSR values

--- src/main.cpp
+++ src/main.cpp
@@ -8,122 +8,82 @@
   gyroscope, and magnetometer data out. Added display functions to allow display
   to on breadboard monitor.
 */
-#include <M5Stack.h>
-#include "utility/MPU9250.h"
+class Deque {
+private:
+    int theList[280] = {};
+    int headIdx = 0;
+    void incrementHead()
+    {
+        if (headIdx == 280)
+            headIdx = 0;
+        else
+            headIdx++;
+    }
 
-MPU9250 IMU;
+public:
+
+    void push(int newVal)
+    {
+        theList[headIdx] = newVal;
+        incrementHead();
+    }
+
+    int read(int idx)
+    {
+        int readVal;
+        int x = headIdx + idx;
+        if (x > 280) x = x - 280;
+
+        readVal = theList[x];
+        return readVal;
+    }
+
+};
+
+#include <M5Stack.h>
+
+#define TFT_GREY 0x7BEF
+
+const int GSR = 35;
+int sensorValue = 0;
+int gsr_average = 0;
+Deque gsrQueue;
 
 void setup()
 {
     M5.begin();
     Wire.begin();
-    SD.open("/hello.txt");
-    Serial.begin(115200);
+    Serial.begin(9600);
 
-    IMU.calibrateMPU9250(IMU.gyroBias, IMU.accelBias);
-    IMU.initMPU9250();
-    IMU.initAK8963(IMU.magCalibration);
+    for(int i=0; i<280; i++) gsrQueue.push(100);
 }
 
 void loop()
 {
-    // If intPin goes high, all data registers have new data
-    // On interrupt, check if data ready interrupt
-    if (IMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
+    long sum = 0;
+    for (int i = 0; i < 10; i++) //Average the 10 measurements to remove the glitch
     {
-        IMU.readAccelData(IMU.accelCount);
-        IMU.getAres();
-
-        IMU.ax = (float)IMU.accelCount[0] * IMU.aRes; // - accelBias[0];
-        IMU.ay = (float)IMU.accelCount[1] * IMU.aRes; // - accelBias[1];
-        IMU.az = (float)IMU.accelCount[2] * IMU.aRes; // - accelBias[2];
-
-        IMU.readGyroData(IMU.gyroCount); // Read the x/y/z adc values
-        IMU.getGres();
-
-        // Calculate the gyro value into actual degrees per second
-        // This depends on scale being set
-        IMU.gx = (float)IMU.gyroCount[0] * IMU.gRes;
-        IMU.gy = (float)IMU.gyroCount[1] * IMU.gRes;
-        IMU.gz = (float)IMU.gyroCount[2] * IMU.gRes;
-
-        IMU.readMagData(IMU.magCount); // Read the x/y/z adc values
-        IMU.getMres();
-        // User environmental x-axis correction in milliGauss, should be
-        // automatically calculated
-        //IMU.magbias[0] = +470.;
-        // User environmental x-axis correction in milliGauss TODO axis??
-        //IMU.magbias[1] = +120.;
-        // User environmental x-axis correction in milliGauss
-        //IMU.magbias[2] = +125.;
-
-        // Calculate the magnetometer values in milliGauss
-        // Include factory calibration per data sheet and user environmental
-        // corrections
-        // Get actual magnetometer value, this depends on scale being set
-        IMU.mx = (float)IMU.magCount[0] * IMU.mRes * IMU.magCalibration[0] -
-                 IMU.magbias[0];
-        IMU.my = (float)IMU.magCount[1] * IMU.mRes * IMU.magCalibration[1] -
-                 IMU.magbias[1];
-        IMU.mz = (float)IMU.magCount[2] * IMU.mRes * IMU.magCalibration[2] -
-                 IMU.magbias[2];
-
-        IMU.tempCount = IMU.readTempData(); // Read the adc values
-        // Temperature in degrees Centigrade
-        IMU.temperature = ((float)IMU.tempCount) / 333.87 + 21.0;
-
-        int x = 64 + 10;
-        int y = 128 + 20;
-        int z = 192 + 30;
-
-        M5.Lcd.fillScreen(BLACK);
-        M5.Lcd.setTextColor(GREEN, BLACK);
-        M5.Lcd.setTextSize(2);
-        M5.Lcd.setCursor(0, 0);
-        M5.Lcd.print("MPU9250/AK8963");
-        M5.Lcd.setCursor(0, 32);
-        M5.Lcd.print("x");
-        M5.Lcd.setCursor(x, 32);
-        M5.Lcd.print("y");
-        M5.Lcd.setCursor(y, 32);
-        M5.Lcd.print("z");
-
-        M5.Lcd.setTextColor(YELLOW, BLACK);
-        M5.Lcd.setCursor(0, 48 * 2);
-        M5.Lcd.print((int)(1000 * IMU.ax));
-        M5.Lcd.setCursor(x, 48 * 2);
-        M5.Lcd.print((int)(1000 * IMU.ay));
-        M5.Lcd.setCursor(y, 48 * 2);
-        M5.Lcd.print((int)(1000 * IMU.az));
-        M5.Lcd.setCursor(z, 48 * 2);
-        M5.Lcd.print("mg");
-
-        M5.Lcd.setCursor(0, 64 * 2);
-        M5.Lcd.print((int)(IMU.gx));
-        M5.Lcd.setCursor(x, 64 * 2);
-        M5.Lcd.print((int)(IMU.gy));
-        M5.Lcd.setCursor(y, 64 * 2);
-        M5.Lcd.print((int)(IMU.gz));
-        M5.Lcd.setCursor(z, 64 * 2);
-        M5.Lcd.print("o/s");
-
-        M5.Lcd.setCursor(0, 80 * 2);
-        M5.Lcd.print((int)(IMU.mx));
-        M5.Lcd.setCursor(x, 80 * 2);
-        M5.Lcd.print((int)(IMU.my));
-        M5.Lcd.setCursor(y, 80 * 2);
-        M5.Lcd.print((int)(IMU.mz));
-        M5.Lcd.setCursor(z, 80 * 2);
-        M5.Lcd.print("mG");
-
-        M5.Lcd.setCursor(0, 96 * 2);
-        M5.Lcd.print("Gyro Temperature ");
-        M5.Lcd.setCursor(z, 96 * 2);
-        M5.Lcd.print(IMU.temperature, 1);
-        M5.Lcd.print(" C");
-
-        Serial.println((int)(1000 * IMU.gx));
-
-        delay(100);
+        sensorValue = analogRead(GSR);
+        sum += sensorValue;
+        delay(5);
     }
+    gsr_average = sum / 10;
+
+    M5.Lcd.fillScreen(TFT_BLACK);                   // Clear and reset the screen
+    M5.Lcd.drawLine(40, 0, 40, 200, YELLOW);        // Draw y-axis for Graph
+    M5.Lcd.drawLine(40, 200, 320, 200, YELLOW);     // Draw x-axis for Graph
+
+    long pos = map(gsr_average, 0, 4095, 200, 0);   // Mapping GSR values to screen height
+    int posInt = floor(pos);
+    gsrQueue.push(posInt);
+
+    for(int i=0; i<280; i++)
+    {
+    int graphPos = gsrQueue.read(i);
+    M5.Lcd.drawPixel(i + 40, graphPos, YELLOW);
+    }
+
+    M5.Lcd.setTextColor(YELLOW, BLACK);
+    M5.Lcd.setCursor(10, posInt);
+    M5.Lcd.print((int)(gsr_average));
 }
