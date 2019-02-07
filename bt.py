7ea4a49f1306a719d1f3e0417a9ff5eab35d766f
nebbles <bsg115@ic.ac.uk>
2019-02-07 14:32:08 +0000

    Initial work with bluetooth communication

---
+++ bt.py
@@ -0,0 +1,19 @@
+import time
+import serial
+
+with serial.Serial('/dev/cu.ESP32_LED_Control-ESP32', 115200) as ser:
+    print("Establishing connection to device...", flush=True)
+    time.sleep(2)
+    i = 0
+    while i < 100:
+        msg = "hello"
+        print("Sending message over serial... {}".format(msg), flush=True)
+        ser.write("Hello".encode())
+        print("Sent.")
+        time.sleep(1)
+        i+= 0
+
+# with serial.Serial('/dev/cu.usbmodem14201', 57600) as ser:
+#     while True:
+#         line = ser.readline()   # read a '\n' terminated line
+#         print(line)
