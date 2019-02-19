51619356cddcf0f461d8b7c504af37d9c9e1cd94
leahpattison <leah.pattison@hotmail.co.uk>
2019-02-19 12:15:27 +0000

    App push 1
    Log screen complete & exporting to json

---
+++ app/Assets/Scripts/sliderValues.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class sliderValues : MonoBehaviour
+{
+	public Slider Anxious;
+	public Slider Stressed; 
+	public Slider Tired;
+	public Slider Productivity;
+
+
+
+	//Invoked when a submit button is clicked.
+	public void SubmitSliderSetting()
+	{
+		//Displays the value of the slider in the console.
+		Debug.Log(Anxious.value);
+		//return JsonUtility.ToJson(this);
+	}
+}
