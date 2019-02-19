70fa6cbca7c72c50c0acd272c74cb4c25dca4631
Scott-Bunting <scott.bunting15@imperial.ac.uk>
2019-02-19 17:56:55 +0000

    Separation of code into header file
    Reduces the functionality of the graph due to while loop.
    Will be amended with two functions: ppgInter, ppgCalc.

--- src/main.cpp
+++ src/main.cpp
@@ -29,69 +29,18 @@
 *
 * ------------------------------------------------------------------------- */
 
-class Deque {
-private:
-    int theList[280] = {};
-    int headIdx = 0;
-    void incrementHead()
-    {
-        if (headIdx == 280)
-            headIdx = 0;
-        else
-            headIdx++;
-    }
-
-public:
-
-    void push(int newVal)
-    {
-        theList[headIdx] = newVal;
-        incrementHead();
-    }
-
-    int read(int idx)
-    {
-        int readVal;
-        int x = headIdx + idx;
-        if (x > 280) x = x - 280;
-
-        readVal = theList[x];
-        return readVal;
-    }
-
-};
-
 #include <M5Stack.h>
-#include <Wire.h>
-#include <SPI.h>
-#include "algorithm_by_RF.h"
-#include "max30102.h"
-Deque ppgQueue;
-
-// #define DEBUG // Uncomment for debug output to the Serial stream
-//#define SCREEN //Uncomment to show debug data on M5 Stack Screen
-#define TEST_MAXIM_ALGORITHM // Uncomment if you want to include results returned by the original MAXIM algorithm
-//#define SAVE_RAW_DATA // Uncomment if you want raw data coming out of the sensor saved to SD card. Red signal first, IR second.
-
-#ifdef TEST_MAXIM_ALGORITHM
-  #include "algorithm.h" 
-#endif
-
-// Interrupt pin
-const byte oxiInt = 5; // pin connected to MAX30102 INT
-
-uint32_t elapsedTime, timeStart;
-
-uint32_t aun_ir_buffer[BUFFER_SIZE]; //infrared LED sensor data
-uint32_t aun_red_buffer[BUFFER_SIZE];  //red LED sensor data
-float old_n_spo2;  // Previous SPO2 value
-uint8_t uch_dummy,k;
+#include "ppg.h"
 
 void setup() {
 
   M5.begin();
-  pinMode(oxiInt, INPUT);  //pin G5 connects to the interrupt output pin of the MAX30102
   Wire.begin();
+
+#if defined(DEBUG) || !defined(USE_ADALOGGER)
+  // initialize serial communication at 115200 bits per second:
+  Serial.begin(115200);
+#endif
 
   // Write text to show that PPG Script is Running
   M5.Lcd.setTextColor(YELLOW);
@@ -99,183 +48,66 @@ void setup() {
   M5.Lcd.setCursor(0, 0);
   M5.Lcd.print("PPG Script: Started");
 
-  // Draw lines for graph
-  //M5.Lcd.drawLine(40, 20, 40, 220, WHITE);
-  //M5.Lcd.drawLine(40, 220, 320, 220, WHITE);
-
-#if defined(DEBUG) || !defined(USE_ADALOGGER)
-  // initialize serial communication at 115200 bits per second:
-  Serial.begin(115200);
-#endif
-
-  maxim_max30102_reset(); //resets the MAX30102
-  delay(1000);
-
-  maxim_max30102_read_reg(REG_INTR_STATUS_1,&uch_dummy);  //Reads/clears the interrupt status register
-  maxim_max30102_init();  //initialize the MAX30102
-  old_n_spo2=0.0;
+  //Draw lines for graph
+  M5.Lcd.drawLine(40, 20, 40, 220, WHITE);
+  M5.Lcd.drawLine(40, 220, 320, 220, WHITE);
 
   while(Serial.available()==0)  //wait until user presses a key
   {
     Serial.println(F("Press any key to start conversion"));
     delay(1000);
   }
+
+ppgInit();
+
   // Write text to show that Measurement has Started
   M5.Lcd.setTextColor(YELLOW, BLACK);
   M5.Lcd.setTextSize(2);
   M5.Lcd.setCursor(0, 0);
   M5.Lcd.print("PPG Script: Serial Running");
-  uch_dummy=Serial.read();
-#ifdef TEST_MAXIM_ALGORITHM
-  Serial.print(F("Time[s]\tSpO2\tHR\tSpO2_MX\tHR_MX\tClock\tRatio\tCorr"));
-#else // TEST_MAXIM_ALGORITHM
-  Serial.print(F("Time[s]\tSpO2\tHR\tClock\tRatio\tCorr"));
-#endif // TEST_MAXIM_ALGORITHM
-#ifdef SAVE_RAW_DATA
-  int32_t i;
-  // These are headers for the red signal
-  for(i=0;i<BUFFER_SIZE;++i) {
-    Serial.print("\t");
-    Serial.print(i);
-  }
-  // These are headers for the infrared signal
-  for(i=0;i<BUFFER_SIZE;++i) {
-    Serial.print("\t");
-    Serial.print(i);
-  }
-#endif // SAVE_RAW_DATA
-  Serial.println("");
+
     
   timeStart=millis();
-
-
-for(int i=0; i<280; i++) ppgQueue.push(100);
 }
 
 //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every ST seconds
-void loop() {
-  float n_spo2,ratio,correl;  //SPO2 value
-  int8_t ch_spo2_valid;  //indicator to show if the SPO2 calculation is valid
-  int32_t n_heart_rate; //heart rate value
-  int8_t  ch_hr_valid;  //indicator to show if the heart rate calculation is valid
-  int32_t i;
-  char hr_str[10];
-     
-  //buffer length of BUFFER_SIZE stores ST seconds of samples running at FS sps
-  //read BUFFER_SIZE samples, and determine the signal range
-  for(i=0;i<BUFFER_SIZE;i++)
-  {
-    while(digitalRead(oxiInt)==1);  //wait until the interrupt pin asserts
-    maxim_max30102_read_fifo((aun_red_buffer+i), (aun_ir_buffer+i));  //read from MAX30102 FIFO
-    Serial.println("Interrupt Loop");
-    Serial.print(aun_red_buffer[i], DEC);
-    Serial.print(F("\n"));
-    float raw_ir = raw_ir_read(aun_ir_buffer, BUFFER_SIZE, aun_red_buffer, &n_spo2, &n_heart_rate);
-    Serial.println((float)(raw_ir));
-    if(raw_ir < 1000 && raw_ir > -1000)
-    {
-        long graphPos = map(raw_ir, -1000, 1000, 200, 0);
-        graphPos = floor(graphPos);
-        ppgQueue.push(graphPos);
-    }
+void loop() 
+{
+ppgRun();
 
-    M5.Lcd.fillRect(41, 20, 280, 200, BLACK);                   // Clear and reset the screen
-    for(int i=0; i<280; i++)
-    {
-    int graphPos = ppgQueue.read(i);
-    if (graphPos > 20 && graphPos < 220) M5.Lcd.drawPixel(i + 41, graphPos, BLUE);  //Temp fix to prevent diagonal line from being drawn
-    }
-    float raw_red = raw_red_read(BUFFER_SIZE, aun_red_buffer, &n_spo2, &n_heart_rate);
-    Serial.println((float)(raw_ir));
-#ifdef DEBUG
-    Serial.print(i, DEC);
-    Serial.print(F("\t"));
-    Serial.print(aun_red_buffer[i], DEC);
-    Serial.print(F("\t"));
-    Serial.print(aun_ir_buffer[i], DEC);    
-    Serial.println("");
-#endif // DEBUG
-  }
+float raw_ir = raw_ir_read(aun_ir_buffer, BUFFER_SIZE, aun_red_buffer, &n_spo2, &n_heart_rate);
+float raw_red = raw_red_read(BUFFER_SIZE, aun_red_buffer, &n_spo2, &n_heart_rate);
+#ifdef showIR
+if(raw_ir < 1000 && raw_ir > -1000) // If IR reflectance values are consistent with HR variance
+{
+    long graphPos = map(raw_ir, -1000, 1000, 200, 0);
+    graphPos = floor(graphPos);
+    ppgQueue.push(graphPos);
+}
+#endif // showIR
 
-  //calculate heart rate and SpO2 after BUFFER_SIZE samples (ST seconds of samples) using Robert's method
-  rf_heart_rate_and_oxygen_saturation(aun_ir_buffer, BUFFER_SIZE, aun_red_buffer, &n_spo2, &ch_spo2_valid, &n_heart_rate, &ch_hr_valid, &ratio, &correl); 
-  elapsedTime=millis()-timeStart;
-  M5.Lcd.setTextColor(RED, BLACK);
-  M5.Lcd.setCursor(0, 30);
-  M5.Lcd.print((int)(elapsedTime/1000));
-  // millis_to_hours(elapsedTime,hr_str); // Time in hh:mm:ss format
-  elapsedTime/=1000; // Time in seconds
+#ifdef showRed
+if(raw_red < 1000 && raw_red > -1000) // If IR reflectance values are consistent with HR variance
+{
+    graphPos = map(raw_red, -1000, 1000, 200, 0);
+    graphPos = floor(graphPos);
+    ppgQueue.push(graphPos);
+}
+#endif // showRed
 
-#ifdef DEBUG
-  Serial.println("--RF--");
-  Serial.print(elapsedTime);
-  Serial.print("\t");
-  Serial.print(n_spo2);
-  Serial.print("\t");
-  Serial.print(n_heart_rate, DEC);
-  Serial.print("\t");
-  Serial.println(hr_str);
-  Serial.println("------");
-#endif // DEBUG
-
-#ifdef TEST_MAXIM_ALGORITHM
-  //calculate heart rate and SpO2 after BUFFER_SIZE samples (ST seconds of samples) using MAXIM's method
-  float n_spo2_maxim;  //SPO2 value
-  int8_t ch_spo2_valid_maxim;  //indicator to show if the SPO2 calculation is valid
-  int32_t n_heart_rate_maxim; //heart rate value
-  int8_t  ch_hr_valid_maxim;  //indicator to show if the heart rate calculation is valid
-  maxim_heart_rate_and_oxygen_saturation(aun_ir_buffer, BUFFER_SIZE, aun_red_buffer, &n_spo2_maxim, &ch_spo2_valid_maxim, &n_heart_rate_maxim, &ch_hr_valid_maxim); 
-#ifdef DEBUG
-  Serial.println("--MX--");
-  Serial.print(elapsedTime);
-  Serial.print("\t");
-  Serial.print(n_spo2_maxim);
-  Serial.print("\t");
-  Serial.print(n_heart_rate_maxim, DEC);
-  Serial.print("\t");
-  Serial.println(hr_str);
-  Serial.println("------");
-#endif // DEBUG
-#endif // TEST_MAXIM_ALGORITHM
-
-  //save samples and calculation result to SD card
-#ifdef TEST_MAXIM_ALGORITHM
-  if(ch_hr_valid && ch_spo2_valid || ch_hr_valid_maxim && ch_spo2_valid_maxim) {
-#else   // TEST_MAXIM_ALGORITHM
-  if(ch_hr_valid && ch_spo2_valid) { 
-#endif // TEST_MAXIM_ALGORITHM
-    Serial.print(elapsedTime);
-    Serial.print("\t");
-    Serial.print(n_spo2);
-    Serial.print("\t");
-    Serial.print(n_heart_rate, DEC);
-    Serial.print("\t");
-#ifdef TEST_MAXIM_ALGORITHM
-    Serial.print(n_spo2_maxim);
-    Serial.print("\t");
-    Serial.print(n_heart_rate_maxim, DEC);
-    Serial.print("\t");
-#endif //TEST_MAXIM_ALGORITHM
-    Serial.print(hr_str);
-    Serial.print("\t");
-    Serial.print(ratio);
-    Serial.print("\t");
-    Serial.print(correl);
-#ifdef SAVE_RAW_DATA
-    // Save raw data for unusual O2 levels
-    for(i=0;i<BUFFER_SIZE;++i)
-    {
-      Serial.print(F("\t"));
-      Serial.print(aun_red_buffer[i], DEC);
-    }
-    for(i=0;i<BUFFER_SIZE;++i)
-    {
-      Serial.print(F("\t"));
-      Serial.print(aun_ir_buffer[i], DEC);    
-    }
-#endif // SAVE_RAW_DATA
-    Serial.println("");
-  }
+M5.Lcd.fillRect(0, 20, 39, 240, BLACK); // Moving value for IR reflectance
+M5.Lcd.setCursor(0, graphPos);
+M5.Lcd.print((int)(graphPos));
+M5.Lcd.fillRect(41, 20, 280, 200, BLACK);                   // Clear and reset the screen
+for(int i=0; i<280; i++)
+{
+int graphPos = ppgQueue.read(i);
+if (graphPos > 20 && graphPos < 220) M5.Lcd.drawPixel(i + 41, graphPos, BLUE);  //Temp fix to prevent diagonal line from being drawn
+}
+elapsedTime=millis()-timeStart;
+M5.Lcd.setTextColor(RED, BLACK);
+M5.Lcd.setCursor(0, 30);
+M5.Lcd.print((int)(elapsedTime/1000));
 }
 
 void millis_to_hours(uint32_t ms, char* hr_str)
---
+++ src/ppg.h
@@ -0,0 +1,187 @@
+#ifndef ppg_h
+#define ppg_h
+
+class Deque {
+private:
+    int theList[280] = {};
+    int headIdx = 0;
+    void incrementHead()
+    {
+        if (headIdx == 280)
+            headIdx = 0;
+        else
+            headIdx++;
+    }
+
+public:
+
+    void push(int newVal)
+    {
+        theList[headIdx] = newVal;
+        incrementHead();
+    }
+
+    int read(int idx)
+    {
+        int readVal;
+        int x = headIdx + idx;
+        if (x > 280) x = x - 280;
+
+        readVal = theList[x];
+        return readVal;
+    }
+
+};
+
+#include <M5Stack.h>
+#include <Wire.h>
+#include <SPI.h>
+#include "algorithm_by_RF.h"
+#include "max30102.h"
+Deque ppgQueue;
+
+// #define DEBUG // Uncomment for debug output to the Serial stream
+#define TEST_MAXIM_ALGORITHM // Uncomment if you want to include results returned by the original MAXIM algorithm
+#define showRed
+// #define showIR
+
+#ifdef TEST_MAXIM_ALGORITHM
+  #include "algorithm.h" 
+#endif
+
+// Interrupt pin
+const byte oxiInt = 5; // pin connected to MAX30102 INT
+
+uint32_t elapsedTime, timeStart;
+uint32_t aun_ir_buffer[BUFFER_SIZE]; //infrared LED sensor data
+uint32_t aun_red_buffer[BUFFER_SIZE];  //red LED sensor data
+float old_n_spo2;  // Previous SPO2 value
+uint8_t uch_dummy,k;
+
+// Variables from previous loop function
+float n_spo2,ratio,correl;  //SPO2 value
+int8_t ch_spo2_valid;  //indicator to show if the SPO2 calculation is valid
+int32_t n_heart_rate; //heart rate value
+int8_t  ch_hr_valid;  //indicator to show if the heart rate calculation is valid
+int32_t i;
+char hr_str[10];
+long graphPos;
+
+void ppgInit()
+{
+pinMode(oxiInt, INPUT);  //pin G5 connects to the interrupt output pin of the MAX30102
+maxim_max30102_reset(); //resets the MAX30102
+delay(1000);
+
+  maxim_max30102_read_reg(REG_INTR_STATUS_1,&uch_dummy);  //Reads/clears the interrupt status register
+  maxim_max30102_init();  //initialize the MAX30102
+  old_n_spo2=0.0;
+
+  uch_dummy=Serial.read();
+  
+#ifdef TEST_MAXIM_ALGORITHM
+  Serial.print(F("Time[s]\tSpO2\tHR\tSpO2_MX\tHR_MX\tClock\tRatio\tCorr"));
+#else // TEST_MAXIM_ALGORITHM
+  Serial.print(F("Time[s]\tSpO2\tHR\tClock\tRatio\tCorr"));
+#endif // TEST_MAXIM_ALGORITHM
+
+  Serial.println("");
+
+  for(int i=0; i<280; i++) ppgQueue.push(100);
+}
+
+void ppgRun()
+{
+     
+  //buffer length of BUFFER_SIZE stores ST seconds of samples running at FS sps
+  //read BUFFER_SIZE samples, and determine the signal range
+  for(i=0;i<BUFFER_SIZE;i++)
+  {
+    while(digitalRead(oxiInt)==1);  //wait until the interrupt pin asserts
+    maxim_max30102_read_fifo((aun_red_buffer+i), (aun_ir_buffer+i));  //read from MAX30102 FIFO
+    float raw_ir = raw_ir_read(aun_ir_buffer, BUFFER_SIZE, aun_red_buffer, &n_spo2, &n_heart_rate);
+    float raw_red = raw_red_read(BUFFER_SIZE, aun_red_buffer, &n_spo2, &n_heart_rate);
+
+#ifdef DEBUG
+    Serial.println("Interrupt Loop");
+    Serial.print(aun_red_buffer[i], DEC);
+    Serial.print(F("\n"));
+    Serial.println((float)(raw_ir));
+    Serial.println((float)(raw_red));
+
+
+    Serial.print(i, DEC);
+    Serial.print(F("\t"));
+    Serial.print(aun_red_buffer[i], DEC);
+    Serial.print(F("\t"));
+    Serial.print(aun_ir_buffer[i], DEC);    
+    Serial.println("");
+#endif // DEBUG
+  }
+
+  //calculate heart rate and SpO2 after BUFFER_SIZE samples (ST seconds of samples) using Robert's method
+  rf_heart_rate_and_oxygen_saturation(aun_ir_buffer, BUFFER_SIZE, aun_red_buffer, &n_spo2, &ch_spo2_valid, &n_heart_rate, &ch_hr_valid, &ratio, &correl);
+  // millis_to_hours(elapsedTime,hr_str); // Time in hh:mm:ss format
+  elapsedTime=millis()-timeStart;
+  elapsedTime/=1000; // Time in seconds
+
+#ifdef DEBUG
+  Serial.println("--RF--");
+  Serial.print(elapsedTime);
+  Serial.print("\t");
+  Serial.print(n_spo2);
+  Serial.print("\t");
+  Serial.print(n_heart_rate, DEC);
+  Serial.print("\t");
+  Serial.println(hr_str);
+  Serial.println("------");
+#endif // DEBUG
+
+#ifdef TEST_MAXIM_ALGORITHM
+  //calculate heart rate and SpO2 after BUFFER_SIZE samples (ST seconds of samples) using MAXIM's method
+  float n_spo2_maxim;  //SPO2 value
+  int8_t ch_spo2_valid_maxim;  //indicator to show if the SPO2 calculation is valid
+  int32_t n_heart_rate_maxim; //heart rate value
+  int8_t  ch_hr_valid_maxim;  //indicator to show if the heart rate calculation is valid
+  maxim_heart_rate_and_oxygen_saturation(aun_ir_buffer, BUFFER_SIZE, aun_red_buffer, &n_spo2_maxim, &ch_spo2_valid_maxim, &n_heart_rate_maxim, &ch_hr_valid_maxim); 
+#ifdef DEBUG
+  Serial.println("--MX--");
+  Serial.print(elapsedTime);
+  Serial.print("\t");
+  Serial.print(n_spo2_maxim);
+  Serial.print("\t");
+  Serial.print(n_heart_rate_maxim, DEC);
+  Serial.print("\t");
+  Serial.println(hr_str);
+  Serial.println("------");
+#endif // DEBUG
+#endif // TEST_MAXIM_ALGORITHM
+
+  //save samples and calculation result to SD card
+#ifdef TEST_MAXIM_ALGORITHM
+  if(ch_hr_valid && ch_spo2_valid || ch_hr_valid_maxim && ch_spo2_valid_maxim) {
+#else   // TEST_MAXIM_ALGORITHM
+  if(ch_hr_valid && ch_spo2_valid) { 
+#endif // TEST_MAXIM_ALGORITHM
+    Serial.print(elapsedTime);
+    Serial.print("\t");
+    Serial.print(n_spo2);
+    Serial.print("\t");
+    Serial.print(n_heart_rate, DEC);
+    Serial.print("\t");
+#ifdef TEST_MAXIM_ALGORITHM
+    Serial.print(n_spo2_maxim);
+    Serial.print("\t");
+    Serial.print(n_heart_rate_maxim, DEC);
+    Serial.print("\t");
+#endif //TEST_MAXIM_ALGORITHM
+    Serial.print(hr_str);
+    Serial.print("\t");
+    Serial.print(ratio);
+    Serial.print("\t");
+    Serial.print(correl);
+    Serial.println("");
+  }
+}
+
+#endif // ppg_h
