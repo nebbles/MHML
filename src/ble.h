4a9d9360bd276d76f827b2099ab3fb65e741c0b9
nebbles <bsg115@ic.ac.uk>
2019-02-19 15:52:43 +0000

    Update BLE to support latest spec

--- src/ble.h
+++ src/ble.h
@@ -7,26 +7,36 @@
 #ifndef ble_h
 #define ble_h
 
-BLEServer *pServer = NULL;
-BLECharacteristic *pCharacteristic = NULL;
-BLECharacteristic *pBSLC = NULL;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
-uint32_t value = 0;
+String firmwareRevision = "0.1.0";
 
 // UUIDs specified by the Bluetooth GATT Specification: https://www.bluetooth.com/specifications/gatt
-#define SERVICE_UUID_DEVICE_INFORMATION "0000180A-0000-1000-8000-00805f9b34fb" // Device Information Service (official)
-#define CHARACTERISTIC_UUID_HR "00002A37-0000-1000-8000-00805f9b34fb"          // Heart Rate Measurement (official) 
+#define SERVICE_UUID_DEVICE_INFORMATION "0000180A-0000-1000-8000-00805f9b34fb"  // Device Information Service (official)
+#define CHARACTERISTIC_UUID_FIRMWARE_REV "00002A26-0000-1000-8000-00805f9b34fb" // Firmware Revision (official)
+#define CHARACTERISTIC_UUID_HR "00002A37-0000-1000-8000-00805f9b34fb"           // Heart Rate Measurement (official)
 #define CHARACTERISTIC_UUID_BSL "00002A38-0000-1000-8000-00805f9b34fb"          // Body Sensor Location (official)
 
 // For custom UUIDs, see the following for generating: https://www.uuidgenerator.net/
 #define SERVICE_UUID_PPG "1a632076-8702-41b9-bcff-ea119ae68a69"       // PPG Sensor (custom)
-// #define CHARACTERISTIC_UUID_HR "6bb32e9e-41fd-4abc-8089-f24dbe18aa61" // Heart rate in BPM (custom)
-#define CHARACTERISTIC_UUID_O2 "ef4684bb-c958-40df-90be-5eaa65e07948" // Oxygen level of blood (custom)
 #define SERVICE_UUID_GSR "720f8954-ace5-41f7-acec-113b274bc54f"       // GSR sensor (custom)
+#define CHARACTERISTIC_UUID_O2 "ef4684bb-c958-40df-90be-5eaa65e07948" // Oxygen level of blood (custom)
 #define CHARACTERISTIC_UUID_SR "3f18d911-bffd-4236-b5fc-94c9bf27d0e8" // Skin resistance level (custom)
+// #define CHARACTERISTIC_UUID_OTHER "6bb32e9e-41fd-4abc-8089-f24dbe18aa61" //  (custom)
 
-class MyServerCallbacks : public BLEServerCallbacks
+BLEServer *pServer = NULL;
+BLEService *pServicePPG = NULL;
+BLEService *pServiceGSR = NULL;
+BLEService *pServiceDevInfo = NULL;
+BLECharacteristic *pCharacteristicPPG_HR = NULL;
+BLECharacteristic *pCharacteristicPPG_SPO2 = NULL;
+BLECharacteristic *pCharacteristicPPG_BSL = NULL;
+BLECharacteristic *pCharacteristicGSR_SR = NULL;
+BLECharacteristic *pCharacteristicGSR_BSL = NULL;
+BLECharacteristic *pCharacteristicFR = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint32_t dummyHeartRate = 0;
+
+class customServerCallbacks : public BLEServerCallbacks
 {
     void onConnect(BLEServer *pServer)
     {
@@ -46,35 +56,60 @@ void bleInit()
 
     // Create the BLE Server
     pServer = BLEDevice::createServer();
-    pServer->setCallbacks(new MyServerCallbacks());
+    pServer->setCallbacks(new customServerCallbacks());
 
-    // Create the BLE Service
-    BLEService *pService = pServer->createService(SERVICE_UUID_PPG);
+    /*
+    Create the BLE Service
+    */
+    pServicePPG = pServer->createService(SERVICE_UUID_PPG);
+    pServiceGSR = pServer->createService(SERVICE_UUID_GSR);
+    pServiceDevInfo = pServer->createService(SERVICE_UUID_DEVICE_INFORMATION);
 
-    // Create a BLE Characteristic
-    pCharacteristic = pService->createCharacteristic(
+    /*
+    Create a BLE Characteristics for each service
+    */
+    pCharacteristicPPG_HR = pServicePPG->createCharacteristic(
         CHARACTERISTIC_UUID_HR,
-        BLECharacteristic::PROPERTY_READ |
-            BLECharacteristic::PROPERTY_WRITE |
-            BLECharacteristic::PROPERTY_NOTIFY |
-            BLECharacteristic::PROPERTY_INDICATE);
+        BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
 
-    // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
-    // Create a BLE Descriptor
-    pCharacteristic->addDescriptor(new BLE2902());
+    pCharacteristicPPG_SPO2 = pServicePPG->createCharacteristic(
+        CHARACTERISTIC_UUID_O2,
+        BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
 
-    pBSLC = pService->createCharacteristic(
+    pCharacteristicPPG_BSL = pServicePPG->createCharacteristic(
         CHARACTERISTIC_UUID_BSL,
         BLECharacteristic::PROPERTY_READ);
 
-    // Start the service
-    pService->start();
+    pCharacteristicGSR_SR = pServiceGSR->createCharacteristic(
+        CHARACTERISTIC_UUID_SR,
+        BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
 
-    // Start advertising
+    pCharacteristicGSR_BSL = pServiceGSR->createCharacteristic(
+        CHARACTERISTIC_UUID_BSL,
+        BLECharacteristic::PROPERTY_READ);
+
+    pCharacteristicFR = pServiceDevInfo->createCharacteristic(
+        CHARACTERISTIC_UUID_FIRMWARE_REV,
+        BLECharacteristic::PROPERTY_READ);
+
+    /*
+    All characterstics with the the Notify property must also have the CCC (Client Characteristic Configuration) descriptor.
+
+    https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+    */
+    pCharacteristicPPG_HR->addDescriptor(new BLE2902());
+    pCharacteristicPPG_SPO2->addDescriptor(new BLE2902());
+    pCharacteristicGSR_SR->addDescriptor(new BLE2902());
+
+    // Start the service and then advertise
+    pServicePPG->start();
+    pServiceGSR->start();
+    pServiceDevInfo->start();
+
     BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
     pAdvertising->addServiceUUID(SERVICE_UUID_PPG);
     pAdvertising->setScanResponse(false);
-    pAdvertising->setMinPreferred(0x0); // set value to 0x00 to not advertise this parameter
+    pAdvertising->setMinPreferred(0x0); // set dummyHeartRate to 0x00 to not advertise this parameter
     BLEDevice::startAdvertising();
     Serial.println("Waiting for a client connection to notify...");
 }
@@ -82,22 +117,23 @@ void bleInit()
 void bleRun()
 {
     M5.Lcd.setTextColor(GREEN, BLACK);
-    M5.Lcd.setTextSize(2);
+    M5.Lcd.setTextSize(2); // 20 px high
     M5.Lcd.setCursor(0, 32);
-    M5.Lcd.print(value);
+    M5.Lcd.print(dummyHeartRate);
 
-    // notify changed value
+    // notify changed dummyHeartRate
     if (deviceConnected)
     {
         int bsl = 1;
-        pBSLC->setValue((uint8_t *)&bsl, 4);
-        pCharacteristic->setValue((uint8_t *)&value, 4);
-        pCharacteristic->notify();
-        value++;
-        delay(3); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+        pCharacteristicPPG_BSL->setValue((uint8_t *)&bsl, 4);
+        pCharacteristicFR->setValue(firmwareRevision.c_str());
+        pCharacteristicPPG_HR->setValue((uint8_t *)&dummyHeartRate, 4);
+        pCharacteristicPPG_HR->notify();
+        dummyHeartRate++;
+        delay(5); // bluetooth stack will go into congestion if too many packets are sent; ensure loop is delayed
 
-        Serial.print("Value: 0x");
-        Serial.println(value, HEX);
+        Serial.print("Dummy Heart Rate: 0x");
+        Serial.println(dummyHeartRate, HEX);
         delay(1000);
     }
     // disconnecting
