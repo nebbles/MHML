01c46c5d4342a5d39fee9fb218dc9b83cd22cc31
Cao An <caoan.le15@imperial.ac.uk>
2019-02-26 16:56:17 +0000

    GET handlers for index and teapot route

--- server/main.py
+++ server/main.py
@@ -1,6 +1,12 @@
 from api.flask import app
 import ml.model
 
+@app.route('/api')
+def index():
+    return "MHML server working", 200
+@app.route('/api/teapot')
+def teapot():
+    return "I'm a teapot", 418
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000)
