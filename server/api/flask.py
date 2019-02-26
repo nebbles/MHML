ccfb9d3b5bf7c182f6294197e4fe7e3858098311
Cao An <caoan.le15@imperial.ac.uk>
2019-02-26 15:22:44 +0000

    GET and POST handlers for Users resource, and GET AND DELETE handlers for User resource

--- server/api/flask.py
+++ server/api/flask.py
@@ -2,6 +2,7 @@ import ml.model
 from flask import Flask
 from flask_restful import Api
 from api.resources.stress import Stress
+from api.resources.users import Users, User
 
 class FlaskApp:
     
@@ -9,6 +10,8 @@ class FlaskApp:
         self.app = Flask(__name__)
         self.api = Api(self.app)
         self.api.add_resource(Stress, '/stress')
+        self.api.add_resource(Users, '/api/users')
+        self.api.add_resource(User, '/api/users/<string:username>')
         print("flask app ready!")
 
 
---
+++ server/api/resources/users.py
@@ -0,0 +1,49 @@
+from api.firestore import database
+from flask_restful import Resource, reqparse
+from flask import request
+
+users_ref = database.collection(u'users')
+
+class Users(Resource):
+    
+    def get(self):
+        users = users_ref.get()
+        usernames = []
+        for user in users:
+            username = user.to_dict()['Username']
+            usernames.append(username)
+        return {"usernames": usernames}, 200
+    
+    def post(self):
+        parser = reqparse.RequestParser(bundle_errors=True)
+        parser.add_argument('Username', required=True, help="Missing entry for Username field")
+        parser.add_argument('Name', type=str, help="Invalid type for Name field")
+        parser.add_argument('Age',  type=int, help="Invalid type for Age field")
+        parser.add_argument('Gender', type=bool, help="Invalid type for Gender field")
+        args = dict(parser.parse_args())
+        data = {k: v for k, v in args.items() if v is not None}
+        users_ref.document(args['Username']).set(data)
+        return {"created": data}, 201
+
+class User(Resource):
+
+    def get(self, username):
+        user_ref = users_ref.document(username)
+        user = user_ref.get()
+        if user.exists:
+            return user.to_dict(), 200
+        else:
+            return "ERROR NOT FOUND", 404
+
+    def delete(self, username):
+        user_ref = users_ref.document(username)
+        user = user_ref.get()
+        if user.exists:
+            user_ref.delete()
+            return "DELETED USER WITH USERNAME " + username, 200 
+        else:
+            return "ERROR NOT FOUND", 404
+
+
+
+
