d5c74c1ff898a491f79c97c6aa180defb9e1aa22
mohyaboualam <mta415@ic.ac.uk>
2019-02-27 11:23:31 +0000

    Update receive.cs
    - Changed the web address that the get request is sent to to the real API web address not the mock server that was used previously. 
    - Added comments to explain the different parts of the code for ease of use by other group members.

--- app/Assets/Scripts/Wifi/receive.cs
+++ app/Assets/Scripts/Wifi/receive.cs
@@ -3,25 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-
+//Once we know what data we expect to receive from the API we can create a class for it to be able to handle it and extract 
+//and process each part of the data in the required manner
 public class receive : MonoBehaviour {	
-		public void Upload(){
+		public void Upload(){ //Function attached to a button on the app to retreive data from the API
 			StartCoroutine(GetText());
 		}
 
 		IEnumerator GetText() {
-			UnityWebRequest www = UnityWebRequest.Get("https://meeha.free.beeceptor.com");
+			UnityWebRequest www = UnityWebRequest.Get("mhml.greenberg.io/stress"); //Get request to API
 			yield return www.SendWebRequest();
 
-			if(www.isNetworkError || www.isHttpError) {
-				Debug.Log(www.error);
+			if(www.isNetworkError || www.isHttpError) { //This statement is executed if the get request fails
+				Debug.Log(www.error); //A message is displayed on the console in unity
 			}
-			else {
-				// Show results as text
+			else { //This statement is executed when the get request has been processed successfully. 
+				// Show data retrieved from the API as text in the console on unity
 				Debug.Log(www.downloadHandler.text);
 
 				// Or retrieve results as binary data
-				byte[] results = www.downloadHandler.data;
+				byte[] results = www.downloadHandler.data; 
 			}
 		}
 	}
