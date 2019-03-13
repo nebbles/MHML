2260e43c9fcb4fcfd5e411a3bfdd72638b924364
leahpattison <leah.pattison@hotmail.co.uk>
2019-03-13 01:25:45 +0000

    app directory changes

--- app/Assets/Scripts/Removed/windowGraph.cs
+++ app/Assets/Scripts/Removed/windowGraph.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI ; 
+using UnityEngine.UI ;
+using CodeMonkey.Utils; 
 
 public class windowGraph : MonoBehaviour {
 	[SerializeField] private Sprite circleSprite; 
 	private RectTransform graphContainer;
+    private RectTransform labelTemplateX;
+    private RectTransform labelTemplateY;
 	// Use this for initialization
 	private void Awake() {
 		graphContainer = transform.Find ("graphContainer").GetComponent<RectTransform> ();
-		CreateCircle(new Vector2 (200, 200)); 
+        labelTemplateX = graphContainer.Find("labelTemplateX").GetComponent<RectTransform>();
+        labelTemplateY = graphContainer.Find("labelTemplateY").GetComponent<RectTransform>(); 
+        CreateCircle(new Vector2 (200, 200)); 
+
+        // get data from list 
 		List<int> valueList = new List<int>() {5, 98, 56, 45, 30, 22, 17, 15, 13, 25, 37, 40, 36, 33};
 		ShowGraph(valueList);
 	}
@@ -27,7 +34,8 @@ public class windowGraph : MonoBehaviour {
 
 	private void ShowGraph(List<int> valueList) {
 		float graphHeight = graphContainer.sizeDelta.y;
-		float yMaximum = 100f; 
+		float yMaximum = 4100f; 
+
 		float xSize = 50f;
 
 		GameObject lastCircleGameObject = null;
@@ -39,16 +47,39 @@ public class windowGraph : MonoBehaviour {
 				CreateDotConnection(lastCircleGameObject.GetComponent<RectTransform>().anchoredPosition, circleGameObject.GetComponent<RectTransform>().anchoredPosition); 
 			}
 			lastCircleGameObject = circleGameObject;
+
+            RectTransform labelX = Instantiate(labelTemplateX);
+            labelX.SetParent(graphContainer);
+            labelX.gameObject.SetActive(true);
+            labelX.anchoredPosition = new Vector2(xPosition, -20f);
+            labelX.GetComponent<Text>().text = i.ToString(); 
+            
+
 		}
+
+        int separatorCount = 10; 
+        for (int i =0; i <= separatorCount; i++)
+        {
+            RectTransform labelY = Instantiate(labelTemplateY);
+            labelY.SetParent(graphContainer);
+            labelY.gameObject.SetActive(true);
+            float normalizedValue = i *1f/ separatorCount;
+            labelY.anchoredPosition = new Vector2(-7f, normalizedValue*graphHeight);
+            labelY.GetComponent<Text>().text = Mathf.RoundToInt(normalizedValue*yMaximum).ToString();
+        }
 	}
 
 	private void CreateDotConnection(Vector2 dotPositionA, Vector2 dotPositionB) {
 		GameObject gameObject = new GameObject("dotConnection", typeof(Image));
 		gameObject.transform.SetParent (graphContainer, false);
-		RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
-		rectTransform.anchorMin = new Vector2(0, 0);
+        gameObject.GetComponent<Image>().color = new Color(1, 1, 1, .5f); 
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        Vector2 dir = (dotPositionB - dotPositionA).normalized;
+        float distance = Vector2.Distance(dotPositionA, dotPositionB);
+        rectTransform.anchorMin = new Vector2(0, 0);
 		rectTransform.anchorMax = new Vector2(0, 0);
-		rectTransform.sizeDelta = new Vector2(100, 3f);
-		rectTransform.anchoredPosition = dotPositionA;
-	}
+		rectTransform.sizeDelta = new Vector2(distance, 3f);
+		rectTransform.anchoredPosition = dotPositionA + dir*distance*.5f;
+        rectTransform.localEulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(dir) );
+    }
 }
